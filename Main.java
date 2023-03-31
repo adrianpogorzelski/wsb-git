d5280b7c4d0080d557863583eaa9c9bf5abd3250
Adrian <pogorzelski.adrian@gmail.com>
2023-03-31 22:17:58 +0200

    Zad. 12 z poprzednich zajęć

--- Main.java
+++ Main.java
@@ -6,23 +6,36 @@ import devices.LPG;
 import devices.Petrol;
 
 public class Main {
-    public static void main(String[] args) {
-        Car Electric = new Electric(1, "VW", "Passat", "Diesel", 1.0);
-        Electric.turnOn();
-        Car lpg = new LPG(1, "VW", "Passat", "Diesel", 1.0);
-        lpg.turnOn();
-        Car petrol = new Petrol(1, "VW", "Passat", "Diesel", 2.0);
-        petrol.turnOn();
+    public static void main(String[] args) throws Exception {
+        Car Electric = new Electric(1, "VW", "Passat", "Prond", 1.0);
+        // Electric.turnOn();
+        Car lpg = new LPG(2, "VW", "Passat", "Gaz", 1.0);
+        Car petrol = new Petrol(3, "VW", "Passat", "Diesel", 2.0);
 
         Animal kot = new Animal("kot", 1.0);
 
         Human janusz = new Human("Janusz", kot, 50.0, 2);
         janusz.setCar(1, lpg);
+        Car.salon.setCar(1, petrol);
 
-        System.out.println(janusz.getCar(1));
-        System.out.println(janusz.getCar(-1));
+        //System.out.println(janusz.getCar(1));
+        //System.out.println(janusz.getCar(-1));
 
 
-        System.out.println(janusz.sumGarageValue());
+        //System.out.println(janusz.sumGarageValue());
+
+        Human anrzej = new Human("Anrzej", kot, 100.0, 1);
+
+        System.out.println(petrol.owners);
+        System.out.println("Aktualny wlasciciel Petrol: " + petrol.getCurrentOwner().firstName);
+        petrol.sell(janusz, Car.salon, 20.0);
+
+        System.out.println("Nowy wlasciciel Petrol: " + petrol.getCurrentOwner().firstName);
+
+        System.out.println("Wlasciciele: ");
+        for (int i = 0; i <= petrol.owners.size() - 1; i++) {
+            System.out.println(petrol.owners.get(i).firstName);
+        }
+
     }
 }
--- devices/Car.java
+++ devices/Car.java
@@ -2,15 +2,24 @@ package devices;
 
 import creatures.Human;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public abstract class Car extends Device {
+    public static final Human salon = new Human("Salon", null, 0.0, 10);
     public Double millage;
     public Double engineVolume;
     public String fuelType;
+    public LinkedList<Human> owners;
+    public Human currentOwner;
 
     public Car(Integer id, String producer, String model, String fuelType) {
         super(id, producer, model);
         this.fuelType = fuelType;
+        this.owners = new LinkedList<>();
+        this.owners.add(salon);
     }
+
     public abstract void refuel();
 
     public void turnOn() {
@@ -19,6 +28,8 @@ public abstract class Car extends Device {
         System.out.println("brrr!");
         System.out.println("JeDzZIEmNY!!!");
     }
+
+
 
     public void sell(Human buyer, Human seller, Double price) throws Exception {
         if(!seller.hasCar(this))
@@ -30,10 +41,34 @@ public abstract class Car extends Device {
         if (buyer.cash < price)
             throw new Exception("nie ma piniendzy");
 
+        if (this.getCurrentOwner() != seller)
+            throw new Exception("TO nie jest wlasciciel!!111");
+
         seller.removeCar(this);
         buyer.addCar(this);
         seller.cash += price;
         buyer.cash -= price;
+        this.owners.add(buyer);
         System.out.println("Sprzedano");
     }
+
+    public Human getCurrentOwner() {
+        return this.owners.get(owners.size() - 1);
+    }
+
+    public boolean ownedThisCar(Human human) {
+        return this.owners.contains(human);
+    }
+
+    public boolean didASellToB(Human seller, Human buyer) {
+        Integer indexSeller = this.owners.indexOf(seller);
+        Integer indexBuyer = this.owners.indexOf(buyer);
+
+        if (indexSeller == -1 || indexBuyer == -1)
+            return false;
+
+        return indexBuyer == indexSeller + 1;
+    }
+
+
 }
